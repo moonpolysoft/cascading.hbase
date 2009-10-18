8ce7b0a2b9577cf00c90666a018b1d542fa4a53a
Cliff Moon <cliff@moonpolysoft.com>
2009-10-18 08:57:24 -0700

    ported the tap and scheme to work with the hbase 0.21 api

--- src/java/cascading/hbase/HBaseScheme.java
+++ src/java/cascading/hbase/HBaseScheme.java
@@ -12,22 +12,32 @@
 
 package cascading.hbase;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapred.InputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.OutputFormat;
 
 import cascading.scheme.Scheme;
 import cascading.tap.Tap;
+import cascading.tap.TapException;
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
 import cascading.tuple.TupleEntry;
-import cascading.util.Util;
-import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.mapreduce.*;
-import org.apache.hadoop.hbase.io.*;
-import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.OutputCollector;
 
 /**
  * The HBaseScheme class is a {@link Scheme} subclass. It is used in conjunction
@@ -37,88 +47,88 @@ import org.apache.hadoop.mapred.OutputCollector;
  * @see HBaseTap
  */
 public class HBaseScheme extends Scheme {
-
-  /** Field keyFields */
   private Fields keyField;
-  /** String familyNames */
-  private String[] familyNames;
-  /** Field valueFields */
-  private Fields[] valueFields;
-  /** String columns */
-  private transient String[] columns;
-  /** Field fields */
-  private transient byte[][] fields;
+  private Scan scanner;
+  private Map<String, byte[][]> fieldMap;
+  private Fields valueFields;
 
-  private boolean isFullyQualified = false;
+  // ===========================================================================
+  // Constructors
 
-  /**
-   * Constructor HBaseScheme creates a new HBaseScheme instance.
-   * 
-   * @param keyFields
-   *          of type Fields
-   * @param familyName
-   *          of type String
-   * @param valueFields
-   *          of type Fields
-   */
-  public HBaseScheme(Fields keyFields, String familyName, Fields valueFields) {
-    this(keyFields, new String[] { familyName }, Fields.fields(valueFields));
-  }
-
-  /**
-   * Constructor HBaseScheme creates a new HBaseScheme instance.
-   * 
-   * @param keyFields
-   *          of type Fields
-   * @param familyNames
-   *          of type String[]
-   * @param valueFields
-   *          of type Fields[]
-   */
-  public HBaseScheme(Fields keyFields, String[] familyNames,
-      Fields[] valueFields) {
-    this.keyField = keyFields;
-    // The column Names only holds the family Names.
-    this.familyNames = familyNames;
-    this.valueFields = valueFields;
-
-    setSourceSink(this.keyField, this.valueFields);
-
-    validate();
-  }
-
-  /**
-   * Constructor HBaseScheme creates a new HBaseScheme instance using fully
-   * qualified column names
-   * 
-   * @param keyField
-   *          of type String
-   * @param valueFields
-   *          of type Fields
-   */
-  public HBaseScheme(Fields keyField, Fields valueFields) {
-    this(keyField, Fields.fields(valueFields));
-  }
-
-  /**
-   * Constructor HBaseScheme creates a new HBaseScheme instance using fully
-   * qualified column names
-   * 
-   * @param keyField
-   *          of type Field
-   * @param valueFields
-   *          of type Field[]
-   */
-  public HBaseScheme(Fields keyField, Fields[] valueFields) {
-    // Set a flag that this is using fully qualified names
-    this.isFullyQualified = true;
+  public HBaseScheme(Scan scanner, Fields keyField,
+      Map<String, byte[][]> fieldMap) {
     this.keyField = keyField;
-    this.valueFields = valueFields;
+    this.scanner = scanner;
+    this.fieldMap = fieldMap;
+    this.valueFields = new Fields((Comparable[]) fieldMap.keySet().toArray());
+
+    setSourceSink(keyField, new Fields[] { valueFields });
 
     validate();
+  }
 
-    setSourceSink(this.keyField, this.valueFields);
+  @SuppressWarnings("unchecked")
+  public Tuple source(Object key, Object value) {
+    Tuple result = new Tuple();
 
+    ImmutableBytesWritable keyWritable = (ImmutableBytesWritable) key;
+    Result row = (Result) value;
+
+    result.add(Bytes.toString(keyWritable.get()));
+    
+    for (Comparable c : valueFields) {
+      String fieldName = (String)c;
+      byte[][] cfqual = fieldMap.get(fieldName);
+      byte[] cf = cfqual[0];
+      byte[] qualifier = cfqual[1];
+      byte[] cell = row.getValue(cf, qualifier);
+      result.add(cell);
+    }
+
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  public void sink(TupleEntry tupleEntry, OutputCollector outputCollector)
+      throws IOException {
+    Tuple key = tupleEntry.selectTuple(keyField);
+
+    byte[] keyBytes = Bytes.toBytes(key.getString(0));
+    Put put = new Put(keyBytes);
+    
+    Tuple values = tupleEntry.selectTuple(valueFields);
+    Iterator it = valueFields.iterator();
+    for (int i = 0; it.hasNext(); i++) {
+      String name = (String) it.next();
+      byte[] cell = (byte[]) values.getObject(i);
+      byte[][] cfqual = fieldMap.get(name);
+      byte[] cf = cfqual[0];
+      byte[] qualifier = cfqual[1];
+      put.add(cf, qualifier, cell);
+    }
+
+    outputCollector.collect(null, put);
+  }
+
+  @SuppressWarnings("deprecation")
+  public void sinkInit(Tap tap, JobConf conf) throws IOException {
+    conf
+        .setOutputFormat((Class<? extends OutputFormat>) TableOutputFormat.class);
+
+    conf.setOutputKeyClass(ImmutableBytesWritable.class);
+    conf.setOutputValueClass(Put.class);
+  }
+
+  @SuppressWarnings("deprecation")
+  public void sourceInit(Tap tap, JobConf conf) throws IOException {
+    try {
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      scanner.write(new DataOutputStream(baos));
+      conf.set(TableInputFormat.SCAN, baos.toString());
+      conf.setInputFormat((Class<? extends InputFormat>) TableInputFormat.class);
+    } catch (IOException ex) {
+      throw new TapException(ex);
+    }
   }
 
   private void validate() {
@@ -140,137 +150,12 @@ public class HBaseScheme extends Scheme {
    * 
    * @return the familyNames (type String[]) of this HBaseScheme object.
    */
-  public String[] getFamilyNames() {
-    HashSet<String> familyNameSet = new HashSet<String>();
-    if (isFullyQualified) {
-      for (String columnName : columns(null, this.valueFields)) {
-        int pos = columnName.indexOf(":");
-        familyNameSet.add(hbaseColumn(pos > 0 ? columnName.substring(0, pos)
-            : columnName));
-      }
-    } else {
-      for (String familyName : familyNames) {
-        familyNameSet.add(hbaseColumn(familyName));
-      }
+  public byte[][] getFamilies() {
+    Set<byte[]> families = new HashSet<byte[]>();
+    for (byte[][] cfqual : fieldMap.values()) {
+      families.add(cfqual[0]);
     }
-    return familyNameSet.toArray(new String[0]);
+    
+    return families.toArray(new byte[][] {});
   }
-
-  @SuppressWarnings("unchecked")
-  public Tuple source(Object key, Object value) {
-    Tuple result = new Tuple();
-
-    ImmutableBytesWritable keyWritable = (ImmutableBytesWritable) key;
-    Result row = (Result) value;
-
-    result.add(Bytes.toString(keyWritable.get()));
-
-    for (int i = 0; i < familyNames.length; i++) {
-      Fields cfFields = valueFields[i];
-      for (Comparable comparable : cfFields) {
-        String qualifier = (String) comparable;
-        byte[] cell = row.getValue(familyNames[i].getBytes(), qualifier
-            .getBytes());
-        result.add(cell);
-      }
-    }
-
-    return result;
-  }
-
-  private byte[][] getFieldsBytes() {
-    if (fields == null)
-      fields = makeBytes(this.familyNames, this.valueFields);
-
-    return fields;
-  }
-
-  public void sink(TupleEntry tupleEntry, OutputCollector outputCollector)
-      throws IOException {
-    Tuple key = tupleEntry.selectTuple(keyField);
-
-    byte[] keyBytes = Bytes.toBytes(key.getString(0));
-    BatchUpdate batchUpdate = new BatchUpdate(keyBytes);
-
-    for (int i = 0; i < valueFields.length; i++) {
-      Fields fieldSelector = valueFields[i];
-      TupleEntry values = tupleEntry.selectEntry(fieldSelector);
-
-      for (int j = 0; j < values.getFields().size(); j++) {
-        Fields fields = values.getFields();
-        Tuple tuple = values.getTuple();
-        if (isFullyQualified)
-          batchUpdate.put(hbaseColumn(fields.get(j).toString()), Bytes
-              .toBytes(tuple.getString(j)));
-        else
-          batchUpdate.put(hbaseColumn(familyNames[i])
-              + fields.get(j).toString(), Bytes.toBytes(tuple.getString(j)));
-
-      }
-    }
-
-    outputCollector.collect(null, batchUpdate);
-  }
-
-  public void sinkInit(Tap tap, JobConf conf) throws IOException {
-    conf.setOutputFormat(TableOutputFormat.class);
-
-    conf.setOutputKeyClass(ImmutableBytesWritable.class);
-    conf.setOutputValueClass(BatchUpdate.class);
-  }
-
-  public void sourceInit(Tap tap, JobConf conf) throws IOException {
-    conf.setInputFormat(TableInputFormat.class);
-
-    String columns = getColumns();
-    LOG.debug("sourcing from columns: {}", columns);
-
-    conf.set(TableInputFormat.COLUMN_LIST, columns);
-  }
-
-  private String getColumns() {
-    return Util.join(columns(this.familyNames, this.valueFields), " ");
-  }
-
-  private String[] columns(String[] familyNames, Fields[] fieldsArray) {
-    if (columns != null)
-      return columns;
-
-    int size = 0;
-
-    for (Fields fields : fieldsArray)
-      size += fields.size();
-
-    columns = new String[size];
-
-    for (int i = 0; i < fieldsArray.length; i++) {
-      Fields fields = fieldsArray[i];
-
-      for (int j = 0; j < fields.size(); j++)
-        if (isFullyQualified)
-          columns[i + j] = hbaseColumn((String) fields.get(j));
-        else
-          columns[i + j] = hbaseColumn(familyNames[i]) + (String) fields.get(j);
-    }
-
-    return columns;
-  }
-
-  private byte[][] makeBytes(String[] familyNames, Fields[] fieldsArray) {
-    String[] columns = columns(familyNames, fieldsArray);
-    byte[][] bytes = new byte[columns.length][];
-
-    for (int i = 0; i < columns.length; i++)
-      bytes[i] = Bytes.toBytes(columns[i]);
-
-    return bytes;
-  }
-
-  private String hbaseColumn(String column) {
-    if (column.indexOf(":") < 0)
-      return column + ":";
-    return column;
-
-  }
-
 }
--- src/java/cascading/hbase/HBaseTap.java
+++ src/java/cascading/hbase/HBaseTap.java
@@ -1,20 +1,18 @@
-/*
- * Copyright (c) 2009 Concurrent, Inc.
- *
- * This work has been released into the public domain
- * by the copyright holder. This applies worldwide.
- *
- * In case this is not legally possible:
- * The copyright holder grants any entity the right
- * to use this work for any purpose, without any
- * conditions, unless such conditions are required by law.
- */
-
 package cascading.hbase;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
 
 import cascading.tap.SinkMode;
 import cascading.tap.Tap;
@@ -23,163 +21,126 @@ import cascading.tap.hadoop.TapCollector;
 import cascading.tap.hadoop.TapIterator;
 import cascading.tuple.TupleEntryCollector;
 import cascading.tuple.TupleEntryIterator;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.MasterNotRunningException;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.mapred.TableOutputFormat;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.JobConf;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-/**
- * The HBaseTap class is a {@link Tap} subclass. It is used in conjunction with the {@HBaseFullScheme}
- * to allow for the reading and writing of data to and from a HBase cluster.
- */
-public class HBaseTap extends Tap
-  {
-  /** Field LOG */
-  private static final Logger LOG = LoggerFactory.getLogger( HBaseTap.class );
-
-  /** Field SCHEME */
-  public static final String SCHEME = "hbase";
-
-  /** Field hBaseAdmin */
-  private transient HBaseAdmin hBaseAdmin;
-
-  private String tableName;
-
-  /**
-   * Constructor HBaseTap creates a new HBaseTap instance.
-   *
-   * @param tableName       of type String
-   * @param HBaseFullScheme of type HBaseFullScheme
-   */
-  public HBaseTap( String tableName, HBaseScheme HBaseFullScheme )
-    {
-    super( HBaseFullScheme, SinkMode.APPEND );
+public class HBaseTap extends Tap {
+  public String tableName;
+  
+  //===========================================================================
+  //Constructors
+  
+  public HBaseTap(String tableName, HBaseScheme scheme) {
+    super(scheme, SinkMode.APPEND);
     this.tableName = tableName;
-    }
-
-  /**
-   * Constructor HBaseTap creates a new HBaseTap instance.
-   *
-   * @param tableName       of type String
-   * @param HBaseFullScheme of type HBaseFullScheme
-   * @param sinkMode        of type SinkMode
-   */
-  public HBaseTap( String tableName, HBaseScheme HBaseFullScheme, SinkMode sinkMode )
-    {
-    super( HBaseFullScheme, sinkMode );
+  }
+  
+  public HBaseTap(String tableName, HBaseScheme scheme, SinkMode mode) {
+    super(scheme, mode);
     this.tableName = tableName;
-    }
+  }
+  
+  //===========================================================================
+  //Tap API
+  public boolean deletePath(JobConf conf) {
+    try {
+      HBaseAdmin admin = new HBaseAdmin(new HBaseConfiguration());
 
-  private URI getURI()
-    {
-    try
-      {
-      return new URI( SCHEME, tableName, null );
+      if (!admin.tableExists(tableName)) {
+        return true;
       }
-    catch( URISyntaxException exception )
-      {
-      throw new TapException( "unable to create uri", exception );
-      }
-    }
 
-  public Path getPath()
-    {
-    return new Path( getURI().toString() );
-    }
+      admin.disableTable(tableName);
+      admin.deleteTable(tableName);
 
-  public TupleEntryIterator openForRead( JobConf conf ) throws IOException
-    {
-    return new TupleEntryIterator( getSourceFields(), new TapIterator( this, conf ) );
-    }
-
-  public TupleEntryCollector openForWrite( JobConf conf ) throws IOException
-    {
-    return new TapCollector( this, conf );
-    }
-
-  private HBaseAdmin getHBaseAdmin() throws MasterNotRunningException
-    {
-    if( hBaseAdmin == null )
-      hBaseAdmin = new HBaseAdmin( new HBaseConfiguration() );
-
-    return hBaseAdmin;
-    }
-
-  public boolean makeDirs( JobConf conf ) throws IOException
-    {
-    HBaseAdmin hBaseAdmin = getHBaseAdmin();
-
-    if( hBaseAdmin.tableExists( tableName ) )
       return true;
-
-    LOG.debug( "creating hbase table: {}", tableName );
-
-    HTableDescriptor tableDescriptor = new HTableDescriptor( tableName );
-
-    String[] familyNames = ( (HBaseScheme) getScheme() ).getFamilyNames();
-
-    for( String familyName : familyNames )
-      tableDescriptor.addFamily( new HColumnDescriptor( familyName ) );
-
-    hBaseAdmin.createTable( tableDescriptor );
-
-    return true;
-    }
-
-  public boolean deletePath( JobConf conf ) throws IOException
-    {
-    // eventually keep table meta-data to source table create
-    HBaseAdmin hBaseAdmin = getHBaseAdmin();
-
-    if( !hBaseAdmin.tableExists( tableName ) )
-      return true;
-
-    LOG.debug( "deleting hbase table: {}", tableName );
-
-    hBaseAdmin.disableTable( tableName );
-    hBaseAdmin.deleteTable( tableName );
-
-    return true;
-    }
-
-  public boolean pathExists( JobConf conf ) throws IOException
-    {
-    return getHBaseAdmin().tableExists( tableName );
-    }
-
-  public long getPathModified( JobConf conf ) throws IOException
-    {
-    return System.currentTimeMillis(); // currently unable to find last mod time on a table
-    }
-
-  @Override
-  public void sinkInit( JobConf conf ) throws IOException
-    {
-    LOG.debug( "sinking to table: {}", tableName );
-
-    // do not delete if initialized from within a task
-    if( isReplace() && conf.get( "mapred.task.partition" ) == null )
-      deletePath( conf );
-
-    makeDirs( conf );
-
-    conf.set( TableOutputFormat.OUTPUT_TABLE, tableName );
-    super.sinkInit( conf );
-    }
-
-  @Override
-  public void sourceInit( JobConf conf ) throws IOException
-    {
-    LOG.debug( "sourcing from table: {}", tableName );
-
-    FileInputFormat.addInputPaths( conf, tableName );
-    super.sourceInit( conf );
+    } catch (IOException e) {
+      throw new TapException(e);
     }
   }
+  
+  public Path getPath() {
+    return new Path(getURI().toString());
+  }
+  
+  public long getPathModified() {
+    return System.currentTimeMillis();
+  }
+  
+  public boolean makeDirs(JobConf conf) {
+    HBaseAdmin admin;
+    try {
+      admin = new HBaseAdmin(new HBaseConfiguration());
+
+
+      if (admin.tableExists(tableName)) {
+        return true;
+      }
+
+      HTableDescriptor desc = new HTableDescriptor(tableName);
+      byte[][] columnFamilies = ((HBaseScheme) getScheme()).getFamilies();
+
+      for (byte[] cf : columnFamilies) {
+        HColumnDescriptor cfd = new HColumnDescriptor(cf);
+        desc.addFamily(cfd);
+      }
+
+      admin.createTable(desc);
+
+      return true;
+    } catch (IOException e) {
+      throw new TapException(e);
+    }
+  }
+  
+  public boolean pathExists(JobConf conf) {
+    try {
+      return new HBaseAdmin(new HBaseConfiguration()).tableExists(tableName);
+    } catch (MasterNotRunningException e) {
+      throw new TapException(e);
+    }
+  }
+
+  public long getPathModified(JobConf jobConf) throws IOException {
+    return System.currentTimeMillis();
+  }
+
+  public TupleEntryIterator openForRead(JobConf conf) {
+    try {
+      return new TupleEntryIterator(getSourceFields(), new TapIterator(this, conf));
+    } catch (IOException e) {
+      throw new TapException(e);
+    }
+  }
+  
+  public TupleEntryCollector openForWrite(JobConf conf) {
+    try {
+      return new TapCollector(this, conf);
+    } catch (IOException e) {
+      throw new TapException(e);
+    }
+  }
+  
+  public void sinkInit(JobConf conf) throws IOException {
+    if (isReplace() & conf.get("mapred.task.partition") == null) {
+      deletePath(conf);
+    }
+    
+    makeDirs(conf);
+    
+    conf.set(TableOutputFormat.OUTPUT_TABLE, tableName);
+    super.sinkInit(conf);
+  }
+  
+  public void sourceInit(JobConf conf) throws IOException {
+    conf.set(TableInputFormat.INPUT_TABLE, tableName);
+    super.sinkInit(conf);
+  }
+  //===========================================================================
+  
+  protected URI getURI() {
+    try { 
+      return new URI("hbase://", tableName, null);
+    } catch(URISyntaxException ex) {
+      throw new TapException("unable to create URI", ex);
+    }
+  }
+}
