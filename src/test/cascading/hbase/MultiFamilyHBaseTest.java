f0c689bf21ed112d8c1321d0d3e047a2a3bf6959
Cliff Moon <cliff@moonpolysoft.com>
2009-10-19 15:06:29 -0700

    test the new hbase scheme api

--- src/test/cascading/hbase/MultiFamilyHBaseTest.java
+++ src/test/cascading/hbase/MultiFamilyHBaseTest.java
@@ -31,52 +31,56 @@ import cascading.tuple.Fields;
 /**
  *
  */
-public class MultiFamilyHBaseTest extends HBaseTestCase
-  {
+public class MultiFamilyHBaseTest extends HBaseTestCase {
   transient private static Map<Object, Object> properties = new HashMap<Object, Object>();
 
   String inputFile = "src/test/data/small.txt";
 
-  public MultiFamilyHBaseTest()
-    {
-    super( 1, false );
-    }
+  public MultiFamilyHBaseTest() {
+    super(1, false);
+  }
 
   @Override
-  protected void setUp() throws Exception
-    {
+  protected void setUp() throws Exception {
     super.setUp();
 
-//    MultiMapReducePlanner.setJobConf( properties, conf );
-    }
+    // MultiMapReducePlanner.setJobConf( properties, conf );
+  }
 
-  public void testHBaseMultiFamily() throws IOException
-    {
+  public void testHBaseMultiFamily() throws IOException {
     // create flow to read from local file and insert into HBase
-    Tap source = new Lfs( new TextLine(), inputFile );
+    Tap source = new Lfs(new TextLine(), inputFile);
 
-    Pipe parsePipe = new Each( "insert", new Fields( "line" ), new RegexSplitter( new Fields( "num", "lower", "upper" ), " " ) );
+    Pipe parsePipe = new Each("insert", new Fields("line"), new RegexSplitter(
+        new Fields("num", "lower", "upper"), " "));
 
-    Fields keyFields = new Fields( "num" );
-    String[] familyNames = {"left", "right"};
-    Fields[] valueFields = new Fields[]{new Fields( "lower" ), new Fields( "upper" )};
-    Tap hBaseTap = new HBaseTap( "multitable", new HBaseScheme( keyFields, familyNames, valueFields ), SinkMode.REPLACE );
+    Fields keyFields = new Fields("num");
+    Map<String, byte[][]> fieldMap = new HashMap<String, byte[][]>();
+    fieldMap.put("lower",
+        new byte[][] { "left".getBytes(), "lower".getBytes() });
+    fieldMap.put("upper",
+        new byte[][] { "right".getBytes(), "upper".getBytes() });
+    Tap hBaseTap = new HBaseTap("multitable", new HBaseScheme(keyFields,
+        fieldMap), SinkMode.REPLACE);
 
-    Flow parseFlow = new FlowConnector( properties ).connect( source, hBaseTap, parsePipe );
+    Flow parseFlow = new FlowConnector(properties).connect(source, hBaseTap,
+        parsePipe);
 
     parseFlow.complete();
 
     verifySink( parseFlow, 5 );
 
     // create flow to read from hbase and save to local file
-    Tap sink = new Lfs( new TextLine(), "build/test/multifamily", SinkMode.REPLACE );
+    Tap sink = new Lfs(new TextLine(), "build/test/multifamily",
+        SinkMode.REPLACE);
 
-    Pipe copyPipe = new Each( "read", new Identity() );
+    Pipe copyPipe = new Each("read", new Identity());
 
-    Flow copyFlow = new FlowConnector( properties ).connect( hBaseTap, sink, copyPipe );
+    Flow copyFlow = new FlowConnector(properties).connect(hBaseTap, sink,
+        copyPipe);
 
     copyFlow.complete();
 
-    verifySink( copyFlow, 5 );
-    }
+    verifySink(copyFlow, 5);
   }
+}
